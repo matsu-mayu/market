0915e3108178f35984eae20cf198373edd3758f0
EC2 Default User <ec2-user@ip-172-31-52-51.ec2.internal>
2021-12-01 23:46:57 +0900

    シェイプの変更

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = [
-        'user_id', 'item_id', 'name', 'description', 'price', 'category_id', 'image'
+        'user_id', 'item_id', 'name', 'description', 'price', 'category_id', 'shape_id', 'image'
     ];
 }
