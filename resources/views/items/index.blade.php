fb6c78fb20a91891ed89c56c4d9e6d22c2975de6
student_49384 <kamechans.223@gmail.com>
2022-01-01 21:24:11 +0900

    indexのcss変更

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -117,30 +117,34 @@
             </div>
             <div class="button">
               お気に入りに追加：
-                <a class="like_button">{{ $item->isLikedBy(Auth::user()) ? '★' : '☆' }}</a>
+                <a class="like_button">{{ $item->isLikedBy(Auth::user()) ? '★　' : '☆　' }}</a>
                 <form method="POST" class="like" action="{{ route('items.toggle_like', $item) }}">
                   @csrf
                   @method('patch')
                 </form>
-            </div>       
-            <div>
-              <dl>
-                <dt>商品名</dt>
-                  <dd>{{ $item->name }}</dd>
-                <dt>カテゴリー</dt>
-                  <dd>{{ $item->category->name }}</dd>
-                <dt>性別</dt>
-                  <dd>{{ $item->gender->name }}</dd>
-                <dt>シェイプ</dt>
-                  <dd>{{ $item->shape->name }}</dd>
-                <dt>リムタイプ</dt>
-                  <dd>{{ $item->rim->name }}</dd>
-                <dt>サイズ</dt>
-                  <dd>{{ $item->size->name }}</dd>
-                <dt>カラー</dt>
-                  <dd>{{ $item->color->name }}</dd>
-                <dt>価格</dt>
-                  <dd>{{ $item->price }}円</dd>
+            </div>
+            <div>   
+              <dl class="items_flex">
+                  <div class="items_info">
+                    <dt>商品名</dt>
+                      <dd>{{ $item->name }}</dd>
+                    <dt>カテゴリー</dt>
+                      <dd>{{ $item->category->name }}</dd>
+                    <dt>性別</dt>
+                      <dd>{{ $item->gender->name }}</dd>
+                    <dt>シェイプ</dt>
+                      <dd>{{ $item->shape->name }}</dd>
+                  </div>
+                  <div class="items_info">
+                    <dt>リムタイプ</dt>
+                      <dd>{{ $item->rim->name }}</dd>
+                    <dt>サイズ</dt>
+                      <dd>{{ $item->size->name }}</dd>
+                    <dt>カラー</dt>
+                      <dd>{{ $item->color->name }}</dd>
+                    <dt>価格</dt>
+                      <dd>{{ $item->price }}円</dd>
+                  </div>
               </dl>
             </div>
             <div class="sold_sell">
